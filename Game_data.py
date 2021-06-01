d50ae53f6e9550af56fb73ec212755d1b7a4cc0d
hongsam14 <69339846+hongsam14@users.noreply.github.com>
2021-06-01 20:26:41 +0900

    before pull

--- Game_data.py
+++ Game_data.py
@@ -1,20 +1,9 @@
 import os
-from django.conf import settings
-import Game
+#from django.conf import settings
 import pickle
+import glob
 
 class GameData:
-
-    def __init__(self):
-        #slot<n>_<movieball>_<score>.mmg 파일을 찾음. 
-        pass
-
-    @staticmethod
-    def __load_cache(save_file):
-        _cache = {}
-        with open('cache.pkl', 'rb') as cache:
-            _cache = pickle.load(cache)
-        return _cache
 
     @staticmethod
     def str_builder(id = '', movie_balls = 10, score = 0):
@@ -23,16 +12,51 @@ class GameData:
     @staticmethod
     def save(id = ''):
         try:
-            cache = GameData.__dump_cache()
-            file = open(GameData.str_builder(id, cache['strength'], len(cache['captured']), 'w'))
-            file.write(cache)
-            file.close()
+            cache = GameData.__load_cache()
+            for p in glob.glob('slot'+id+'_[0-9]+_[0-9]+.mmg'):
+                print(p)
+                os.remove(p)
+            with open(GameData.str_builder(id, cache['ball_count'], len(cache['captured'])), 'wb') as file:
+                pickle.dump(cache, file)
             return True
-        except:
+        except Exception as e:
+            print(e)
             return False
-        
+
     @staticmethod
-    def __dump_cache():
+    def __load_cache():
+        _cache = {}
+        with open('cache.pkl', 'rb') as cache:
+            _cache = pickle.load(cache)
+        return _cache
+    
+    @staticmethod
+    def load(id = ''):
+        try:
+            file_name = glob.glob('slot'+id+'_[0-9]+_[0-9]+.mmg')
+            print(file_name)
+            if len(file_name) > 1 or len(file_name) == 0:
+                return False
+            with open(file_name[0], 'rb') as file:
+                GameData.__dump_cache(file)
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    @staticmethod
+    def __dump_cache(_cache):
         _cache = {}
         with open('cache.pkl', 'wb') as cache:
             pickle.dump(_cache, cache)
+    
+# #     # @staticmethod
+# #     # def get_file_list(args):
+
+
+# def main():
+#     print(GameData.save('A'))
+#     print(GameData.load('A'))
+
+# if __name__ == "__main__":
+#     main()
